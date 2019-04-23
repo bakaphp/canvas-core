ed9bb41608f4049889175c1cfc3646d92f70a8fb
Rafael White <rwhite@mctekk.com>
2019-04-23 13:42:47 -0400

    Fix validation of permitted routes

--- src/Middleware/TokenBase.php
+++ src/Middleware/TokenBase.php
@@ -36,8 +36,12 @@ abstract class TokenBase implements MiddlewareInterface
         $calledRoute = $app['router']->getMatchedRoute()->getCompiledPattern();
 
         if (isset($app['userData']) && !Subscription::getPaymentStatus($app->getDI()->getUserData())) {
-            if (array_key_exists($calledRoute, $this->bypassRoutes) && !in_array($request->getMethod(), $this->bypassRoutes[$calledRoute])) {
+            if (!array_key_exists($calledRoute, $this->bypassRoutes)) {
                 throw new UnauthorizedHttpException('Subscription expired,update payment method or verify payment');
+            } else {
+                if (!in_array($request->getMethod(), $this->bypassRoutes[$calledRoute])) {
+                    throw new UnauthorizedHttpException('Subscription expired,update payment method or verify payment');
+                }
             }
         }
 
