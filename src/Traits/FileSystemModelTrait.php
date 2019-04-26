6985d094a8a73cb089de0625516dd61885b63ed2
mctekk <max@mctekk.com>
2019-04-26 00:26:21 -0400

    add filesystem associatioin

--- src/Traits/FileSystemModelTrait.php
+++ src/Traits/FileSystemModelTrait.php
@@ -7,6 +7,7 @@ namespace Canvas\Traits;
 use Baka\Database\Model;
 use Canvas\Models\SystemModules;
 use Canvas\Models\FileSystem;
+use Canvas\Exception\ModelException;
 
 /**
  * Trait ResponseTrait.
@@ -23,8 +24,70 @@ use Canvas\Models\FileSystem;
  */
 trait FileSystemModelTrait
 {
+    public $uploadedFiles = [];
+
     /**
-     * Delete all the files from a module
+     * Associated the list of uploaded files to this entity.
+     * 
+     * call on the after saves
+     *
+     * @return void
+     */
+    protected function associateFileSystem(): bool
+    {
+        foreach ($this->uploadedFiles as $file) {
+            if (!isset($file['id'])) {
+                continue;
+            }
+            if ($file = FileSystem::getById($file['id'])) {
+                $file->entity_id = $this->getId();
+
+                if (!$file->update()) {
+                    throw new ModelException(current($this->getMessages())->getMessage());
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Inserts or updates a model instance. Returning true on success or false otherwise.
+     *
+     *<code>
+     * // Creating a new robot
+     * $robot = new Robots();
+     *
+     * $robot->type = "mechanical";
+     * $robot->name = "Astro Boy";
+     * $robot->year = 1952;
+     *
+     * $robot->save();
+     *
+     * // Updating a robot name
+     * $robot = Robots::findFirst("id = 100");
+     *
+     * $robot->name = "Biomass";
+     *
+     * $robot->save();
+     *</code>
+     *
+     * @param array data
+     * @param array whiteList
+     * @return boolean
+     */
+    public function save($data = null, $whiteList = null): bool
+    {
+        //associate uploaded files
+        if (isset($data['uploadedFiles'])) {
+            $this->uploadedFiles = $data['uploadedFiles'];
+        }
+
+        return parent::save($data, $whiteList);
+    }
+
+    /**
+     * Delete all the files from a module.
      *
      * @return void
      */
