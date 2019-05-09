d0967f6e23a49e578c34e00074462ed8ce6114eb
Rafael White <rwhite@mctekk.com>
2019-05-09 10:13:41 -0400

    Change too beanstalk queue

--- src/Providers/QueueProvider.php
+++ src/Providers/QueueProvider.php
@@ -14,31 +14,31 @@ class QueueProvider implements ServiceProviderInterface
      */
     public function register(DiInterface $container)
     {
-        $container->setShared(
-            'queue',
-            function () {
-                //Connect to the queue
-                $queue = new \PhpAmqpLib\Connection\AMQPStreamConnection(
-                    envValue('RABBITMQ_HOST'),
-                    envValue('RABBITMQ_PORT'),
-                    envValue('RABBITMQ_DEFAULT_USER'),
-                    envValue('RABBITMQ_DEFAULT_PASS')
-                );
-
-                return $queue;
-            }
-        );
-
         // $container->setShared(
         //     'queue',
         //     function () {
         //         //Connect to the queue
-        //         $queue = new Beanstalk([
-        //             'host' => envValue('DATA_API_BEANSTALK_HOST', '127.0.0.1'),
-        //             'prefix' => envValue('DATA_API_BEANSTALK_PORT', 11300),
-        //         ]);
+        //         $queue = new \PhpAmqpLib\Connection\AMQPStreamConnection(
+        //             envValue('RABBITMQ_HOST'),
+        //             envValue('RABBITMQ_PORT'),
+        //             envValue('RABBITMQ_DEFAULT_USER'),
+        //             envValue('RABBITMQ_DEFAULT_PASS')
+        //         );
+
         //         return $queue;
         //     }
         // );
+
+        $container->setShared(
+            'queue',
+            function () {
+                //Connect to the queue
+                $queue = new Beanstalk([
+                    'host' => envValue('DATA_API_BEANSTALK_HOST', '127.0.0.1'),
+                    'prefix' => envValue('DATA_API_BEANSTALK_PORT', 11300),
+                ]);
+                return $queue;
+            }
+        );
     }
 }
