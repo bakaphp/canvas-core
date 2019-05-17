a013344b20afcbf81a6a2787b4f0374ae722d040
Rafael White <rwhite@mctekk.com>
2019-05-17 11:01:59 -0400

    Add API throttle to Canvas

---
+++ src/Middleware/ThrottleMiddleware.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Canvas\Middleware;
+
+use Canvas\Http\Response;
+use Canvas\Traits\ResponseTrait;
+use Phalcon\Mvc\Micro;
+use Phalcon\Mvc\Micro\MiddlewareInterface;
+use Phalcon\Mvc\User\Plugin;
+use OakLabs\PhalconThrottler\ThrottlerInterface;
+use Phalcon\Events\Event;
+use Canvas\Exception\ServerErrorHttpException;
+
+/**
+ * Class ThrottleMiddleware
+ *
+ * @package Canvas\Middleware
+ *
+ * @property Micro    $application
+ * @property Response $response
+ */
+class ThrottleMiddleware extends Plugin implements MiddlewareInterface
+{
+    use ResponseTrait;
+
+    public function call(Micro $api)
+    {
+        /** @var ThrottlerInterface $throttler */
+        $throttler = $this->getDI()->get('throttler');
+        $rateLimit = $throttler->consume($this->request->getClientAddress());
+
+        if ($rateLimit->isLimited()) {
+            throw new ServerErrorHttpException('API Calls limit reached');
+        }
+    }
+}
