9befcf4cb341d4d6589ae6170580c28a4dede12f
mctekk <max@mctekk.com>
2019-06-28 00:32:25 -0400

    remove path for users, it was a test my bad

--- src/Models/Users.php
+++ src/Models/Users.php
@@ -533,9 +533,4 @@ class Users extends \Baka\Auth\Models\Users
     {
         return $this->getAttachementByName('avatar');
     }
-
-    protected function filesNewAttachedPath(): ?string
-    {
-        return 'userss/'.$this->getId();
-    }
 }
