40d9112c9c439045d9b0a5cf139fd84845282673
Rafael White <rwhite@mctekk.com>
2019-07-11 16:04:04 -0400

    Add additional tests for Core config and functions files

--- tests/unit/config/FunctionsCest.php
+++ tests/unit/config/FunctionsCest.php
@@ -3,11 +3,16 @@
 namespace Canvas\Tests\unit\config;
 
 use UnitTester;
+use Codeception\PHPUnit\TestCase;
 use function Canvas\Core\appPath;
 use function Canvas\Core\envValue;
+use function Canvas\Core\isJson;
+use function Canvas\Core\appUrl;
+use function Canvas\Core\paymentGatewayIsActive;
 
 class FunctionsCest
 {
+    // AppPath
     public function checkApppath(UnitTester $I)
     {
         $path = dirname(dirname(dirname(__DIR__)));
@@ -20,6 +25,7 @@ class FunctionsCest
         $I->assertEquals($path, appPath('library/Core/config.php'));
     }
 
+    //EnvValue
     public function checkEnvvalueAsFalse(UnitTester $I)
     {
         putenv('SOMEVAL=false');
@@ -36,5 +42,31 @@ class FunctionsCest
     {
         putenv('SOMEVAL=someval');
         $I->assertEquals('someval', envValue('SOMEVAL'));
+    }
+
+
+    //AppUrl
+    public function checkAppUrlTrue(UnitTester $I)
+    {
+        $I->assertNotEmpty(appUrl('users',1));
+    }
+
+    //IsJson
+    public function checkIsJsonTrue(UnitTester $I)
+    {
+        $array = ['name'=>'example'];
+        $I->assertTrue(isJson(json_encode($array)));
+    }
+
+    public function checkIsJsonFalse(UnitTester $I)
+    {
+        $string = 'example';
+        $I->assertFalse(isJson($string));
+    }
+
+    //PaymentGatewayIsActive
+    public function checkPaymentGatewayIsActiveTrue(UnitTester $I)
+    {
+        $I->assertTrue(paymentGatewayIsActive());
     }
 }
