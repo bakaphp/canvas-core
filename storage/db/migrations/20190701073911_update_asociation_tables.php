44a8657343d5d57b219f83879f23580640b2eadc
mctekk <max@mctekk.com>
2019-07-13 01:00:49 -0400

    fix migration

--- storage/db/migrations/20190701073911_update_asociation_tables.php
+++ storage/db/migrations/20190701073911_update_asociation_tables.php
@@ -189,7 +189,7 @@ class UpdateAsociationTables extends AbstractMigration
             'after' => 'read',
         ])
         ->changeColumn('updated_at', 'datetime', [
-            'null' => true
+            'null' => true,
             'after' => 'created_at',
         ])
         ->changeColumn('is_deleted', 'integer', [
