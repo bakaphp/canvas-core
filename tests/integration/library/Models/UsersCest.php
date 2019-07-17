5fe34108eeb76541683098c3a32d193046dc3a1b
Rafael White <rwhite@mctekk.com>
2019-07-17 17:35:41 -0400

    Add users tests

--- tests/integration/library/Models/UsersCest.php
+++ tests/integration/library/Models/UsersCest.php
@@ -21,27 +21,58 @@ class UsersCest
         $I->assertTrue($app->name == Apps::CANVAS_DEFAULT_APP_NAME);
     }
 
-    // /**
-    //  * Confirm the default apps exist.
-    //  *
-    //  * @param IntegrationTester $I
-    //  * @return void
-    //  */
-    // public function getGewaerApp(IntegrationTester $I)
-    // {
-    //     $app = Apps::getACLApp('Gewaer');
-    //     $I->assertTrue($app->key == $I->grabFromDi('config')->app->id);
-    // }
+    /**
+     * Get Current Company Id
+     *
+     * @param IntegrationTester $I
+     * @return void
+     */
+    public function currentCompanyId(IntegrationTester $I)
+    {
+        $I->assertTrue(gettype($I->grabFromDi('userData')->currentCompanyId()) == 'integer');
+    }
 
-    // /**
-    //  * Validate is an app has an active status or not
-    //  *
-    //  * @param UnitTester $I
-    //  * @return void
-    //  */
-    // public function isActive(IntegrationTester $I)
-    // {
-    //     $app = Apps::getACLApp('Default');
-    //     $I->assertTrue(gettype($app->isActive()) == 'boolean');
-    // }
+    /**
+     * Get Current Company Branch Id
+     *
+     * @param IntegrationTester $I
+     * @return void
+     */
+    public function currentCompanyBranchId(IntegrationTester $I)
+    {
+        $I->assertTrue(gettype($I->grabFromDi('userData')->currentCompanyBranchId()) == 'integer');
+    }
+
+    /**
+     * Get Associated Apps
+     *
+     * @param IntegrationTester $I
+     * @return void
+     */
+    public function getAssociatedApps(IntegrationTester $I)
+    {
+        $I->assertTrue(gettype($I->grabFromDi('userData')->getAssociatedApps()) == 'array');
+    }
+
+    /**
+     * Get Associated Companies
+     *
+     * @param IntegrationTester $I
+     * @return void
+     */
+    public function getAssociatedCompanies(IntegrationTester $I)
+    {
+        $I->assertTrue(gettype($I->grabFromDi('userData')->getAssociatedApps()) == 'array');
+    }
+
+    /**
+     * Get By User Activation Email
+     *
+     * @param IntegrationTester $I
+     * @return void
+     */
+    public function getByUserActivationEmail(IntegrationTester $I)
+    {
+        $I->assertTrue($I->grabFromDi('userData')->getByUserActivationEmail($I->grabFromDi('userData')->user_activation_email) instanceof Users);
+    }
 }
