ef002a4eb3c74ba89428d31f0357e22dd94bf2a6
mctekk <max@mctekk.com>
2019-08-24 23:22:48 -0400

    update migartion to support old kanvas with same tables

--- storage/db/migrations/20190627131332_update_notification_type_table.php
+++ storage/db/migrations/20190627131332_update_notification_type_table.php
@@ -7,15 +7,30 @@ class UpdateNotificationTypeTable extends AbstractMigration
 {
     public function change()
     {
-        $this->table('notification_types', [
-            'id' => false,
-            'primary_key' => ['id'],
-            'engine' => 'InnoDB',
-            'encoding' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'comment' => '',
-            'row_format' => 'DYNAMIC',
-        ])
+        $stmt = $this->query('SHOW COLUMNS FROM notification_types');
+
+        $rows = $stmt->fetchAll();
+
+        $hasColumn = false;
+
+        //verify this columns, how did you get them? who knows
+        //this is for old instances of kanvas
+        foreach ($rows as $row) {
+            if (in_array($row['Field'], ['apps_id', 'system_modules_id', 'description', 'icon_url'])) {
+                $hasColumn = true;
+            }
+        }
+        //work around for old canvas instance running with the old migration
+        if (!$hasColumn) {
+            $this->table('notification_types', [
+                'id' => false,
+                'primary_key' => ['id'],
+                'engine' => 'InnoDB',
+                'encoding' => 'utf8mb4',
+                'collation' => 'utf8mb4_unicode_ci',
+                'comment' => '',
+                'row_format' => 'DYNAMIC',
+            ])
             ->addColumn('apps_id', 'integer', [
                 'null' => false,
                 'limit' => MysqlAdapter::INT_REGULAR,
@@ -26,13 +41,13 @@ class UpdateNotificationTypeTable extends AbstractMigration
                 'limit' => MysqlAdapter::INT_REGULAR,
                 'after' => 'apps_id',
             ])
-        ->changeColumn('name', 'string', [
-            'null' => false,
-            'limit' => 64,
-            'collation' => 'utf8mb4_unicode_ci',
-            'encoding' => 'utf8mb4',
-            'after' => 'system_modules_id',
-        ])
+            ->changeColumn('name', 'string', [
+                'null' => false,
+                'limit' => 64,
+                'collation' => 'utf8mb4_unicode_ci',
+                'encoding' => 'utf8mb4',
+                'after' => 'system_modules_id',
+            ])
             ->addColumn('description', 'text', [
                 'null' => false,
                 'limit' => 65535,
@@ -48,33 +63,35 @@ class UpdateNotificationTypeTable extends AbstractMigration
                 'encoding' => 'utf8mb4',
                 'after' => 'description',
             ])
-        ->changeColumn('created_at', 'datetime', [
-            'null' => false,
-            'after' => 'icon_url',
-        ])
-        ->changeColumn('updated_at', 'datetime', [
-            'null' => true,
-            'after' => 'created_at',
-        ])
-        ->changeColumn('is_deleted', 'integer', [
-            'null' => false,
-            'default' => '0',
-            'limit' => '3',
-            'after' => 'updated_at',
-        ])
-        ->addIndex(['apps_id'], [
-            'name' => 'apps_id',
-            'unique' => false,
-        ])
-        ->addIndex(['system_modules_id'], [
-            'name' => 'system_modules_id',
-            'unique' => false,
-        ])
-        ->addIndex(['apps_id', 'system_modules_id'], [
-            'name' => 'apps_id_system_modules_id',
-            'unique' => false,
-        ])
+            ->changeColumn('created_at', 'datetime', [
+                'null' => false,
+                'after' => 'icon_url',
+            ])
+            ->changeColumn('updated_at', 'datetime', [
+                'null' => true,
+                'after' => 'created_at',
+            ])
+            ->changeColumn('is_deleted', 'integer', [
+                'null' => false,
+                'default' => '0',
+                'limit' => '3',
+                'after' => 'updated_at',
+            ])
+            ->addIndex(['apps_id'], [
+                'name' => 'apps_id',
+                'unique' => false,
+            ])
+            ->addIndex(['system_modules_id'], [
+                'name' => 'system_modules_id',
+                'unique' => false,
+            ])
+            ->addIndex(['apps_id', 'system_modules_id'], [
+                'name' => 'apps_id_system_modules_id',
+                'unique' => false,
+            ])
             ->save();
+        }
+
         $this->table('notifications', [
             'id' => false,
             'primary_key' => ['id'],
