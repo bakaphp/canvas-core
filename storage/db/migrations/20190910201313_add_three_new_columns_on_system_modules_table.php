5ea2beb7684b2141df052a742ee1c9d5e7f4451a
Rafael White <rwhite@mctekk.com>
2019-09-10 16:17:28 -0400

    Add component_type, navigation_type and tab_index to system_modules table

---
+++ storage/db/migrations/20190910201313_add_three_new_columns_on_system_modules_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
+
+class AddThreeNewColumnsOnSystemModulesTable extends AbstractMigration
+{
+    public function change()
+    {
+        $this->table('system_modules', [
+                'id' => false,
+                'primary_key' => ['id'],
+                'engine' => 'InnoDB',
+                'encoding' => 'utf8mb4',
+                'collation' => 'utf8mb4_unicode_ci',
+                'comment' => 'list of modules , user can interact on each of the diff apps',
+                'row_format' => 'DYNAMIC',
+            ])
+        ->addColumn('component_type', 'string', [
+                'null' => true,
+                'limit' => 64,
+                'collation' => 'utf8mb4_unicode_ci',
+                'encoding' => 'utf8mb4',
+                'after' => 'is_deleted',
+        ]),
+        ->addColumn('navigation_type', 'string', [
+            'null' => true,
+            'limit' => 64,
+            'collation' => 'utf8mb4_unicode_ci',
+            'encoding' => 'utf8mb4',
+            'after' => 'name',
+        ])
+        ->addColumn('tab_index', 'integer', [
+                'null' => true,
+                'default' => '0',
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'after' => 'navigation_type',
+            ])
+        ->save();
+    }
+}
