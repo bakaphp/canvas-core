4a960d2c59e7028478a1c0bf2b55c31ae121a357
Rafael White <rwhite@mctekk.com>
2019-09-16 16:59:02 -0400

    Remove old implementation of cache provider

--- src/Providers/CacheDataProvider.php
+++ src/Providers/CacheDataProvider.php
@@ -19,26 +19,11 @@ class CacheDataProvider implements ServiceProviderInterface
         $container->setShared(
             'cache',
             function () {
-                $prefix = 'data';
-                $frontAdapter = Data::class;
-                $frontOptions = [
-                    'lifetime' => envValue('CACHE_LIFETIME', 86400),
-                ];
-                $backOptions = [
-                    'servers' => [
-                        0 => [
-                            'host' => envValue('REDIS_HOST', '127.0.0.1'),
-                            'port' => envValue('REDIS_PORT', 6379)
-                        ],
-                    ],
-                    'client' => [
-                        Redis::OPT_PREFIX_KEY => 'api-',
-                    ],
-                    'lifetime' => 3600,
-                    'prefix' => $prefix . '-',
-                ];
-
-                return new Libmemcached(new $frontAdapter($frontOptions), $backOptions);
+                //Connect to redis
+                $redis = new Redis();
+                $redis->connect(envValue('REDIS_HOST', '127.0.0.1'), (int) envValue('REDIS_PORT', 6379));
+                $redis->setOption(Redis::OPT_SERIALIZER, Redis::SERIALIZER_PHP);
+                return $redis;
             }
         );
     }
