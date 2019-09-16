58dfc8be4586a306e5b9184d0fd4d3da5951584b
Rafael White <rwhite@mctekk.com>
2019-09-16 16:42:34 -0400

    Remove memcached settings

--- src/Providers/CacheDataProvider.php
+++ src/Providers/CacheDataProvider.php
@@ -27,13 +27,11 @@ class CacheDataProvider implements ServiceProviderInterface
                 $backOptions = [
                     'servers' => [
                         0 => [
-                            'host' => envValue('DATA_API_MEMCACHED_HOST', '127.0.0.1'),
-                            'port' => envValue('DATA_API_MEMCACHED_PORT', 11211),
-                            'weight' => envValue('DATA_API_MEMCACHED_WEIGHT', 100),
+                            'host' => envValue('REDIS_HOST', '127.0.0.1'),
+                            'port' => envValue('REDIS_PORT', 6379)
                         ],
                     ],
                     'client' => [
-                        Redis::OPT_HASH => Redis::HASH_MD5,
                         Redis::OPT_PREFIX_KEY => 'api-',
                     ],
                     'lifetime' => 3600,
