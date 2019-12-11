c91691c52bca45970aee873c26a5486e24f8473f
Rafael White <rwhite@mctekk.com>
2019-12-11 11:43:25 -0400

    Add signup and invitation notification types

--- storage/db/seeds/NotificationsSeeder.php
+++ storage/db/seeds/NotificationsSeeder.php
@@ -8,27 +8,11 @@ class NotificationsSeeder extends AbstractSeed
     {
         $data = [
             [
-                'name' => 'System',
+                'name' => 'Signup',
                 'apps_id' => 1,
                 'system_modules_id' => 1,
-                'key' => 'Canvas\\Notifications\\System',
-                'description' => 'System Notification',
-                'created_at' => date('Y-m-d H:m:s')
-            ],
-            [
-                'name' => 'Apps',
-                'apps_id' => 1,
-                'system_modules_id' => 1,
-                'key' => 'Canvas\\Notifications\\Apps',
-                'description' => 'Apps Notification',
-                'created_at' => date('Y-m-d H:m:s')
-            ],
-            [
-                'name' => 'Users',
-                'apps_id' => 1,
-                'system_modules_id' => 1,
-                'key' => 'Canvas\\Notifications\\Users',
-                'description' => 'Users Notification',
+                'key' => 'Canvas\\Notifications\\Signup',
+                'description' => 'Signup Notification',
                 'created_at' => date('Y-m-d H:m:s')
             ],
             [
@@ -37,6 +21,14 @@ class NotificationsSeeder extends AbstractSeed
                 'system_modules_id' => 1,
                 'key' => 'Canvas\\Notifications\\Subscription',
                 'description' => 'Subscription Notification',
+                'created_at' => date('Y-m-d H:m:s')
+            ],
+            [
+                'name' => 'Invitation',
+                'apps_id' => 1,
+                'system_modules_id' => 1,
+                'key' => 'Canvas\\Notifications\\Invitation',
+                'description' => 'Invitation Notification',
                 'created_at' => date('Y-m-d H:m:s')
             ]
         ];
