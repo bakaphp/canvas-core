27478c6704bbf6c38fea79fe71299d82fb46f7f6
Rafael White <rwhite@mctekk.com>
2019-12-11 13:13:23 -0400

    Add system and apps back

--- storage/db/seeds/NotificationsSeeder.php
+++ storage/db/seeds/NotificationsSeeder.php
@@ -8,6 +8,22 @@ class NotificationsSeeder extends AbstractSeed
     {
         $data = [
             [
+                'name' => 'Apps',
+                'apps_id' => 1,
+                'system_modules_id' => 1,
+                'key' => 'Canvas\\Notifications\\Apps',
+                'description' => 'Apps Notification',
+                'created_at' => date('Y-m-d H:m:s')
+            ],
+            [
+                'name' => 'System',
+                'apps_id' => 1,
+                'system_modules_id' => 1,
+                'key' => 'Canvas\\Notifications\\System',
+                'description' => 'System Notification',
+                'created_at' => date('Y-m-d H:m:s')
+            ],
+            [
                 'name' => 'Signup',
                 'apps_id' => 1,
                 'system_modules_id' => 1,
