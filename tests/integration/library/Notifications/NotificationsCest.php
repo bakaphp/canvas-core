7a0fd8fcfbd197cb593d854218d6ba1763209eea
mctekk <max@mctekk.com>
2019-12-25 12:24:00 -0400

    add new test for jobs and notifications

---
+++ tests/integration/library/Notifications/NotificationsCest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Gewaer\Tests\integration\library\Jobs;
+
+use Canvas\Models\Users;
+use Canvas\Notifications\Notify;
+use Canvas\Notifications\PasswordUpdate;
+use IntegrationTester;
+
+class NotificationsCest
+{
+    public function notifyOne(IntegrationTester $I)
+    {
+        $user = Users::findFirst();
+        $user->notify(new PasswordUpdate($user));
+    }
+
+    public function notifyAll(IntegrationTester $I)
+    {
+        $users = Users::find('id in (1|2)');
+        $user = Users::findFirst();
+        Notify::all($users, new PasswordUpdate($user));
+    }
+}
