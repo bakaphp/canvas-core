9dad6ed8f41f0ecefd68d7694d7d332fd097959a
mctekk <max@mctekk.com>
2020-02-21 09:37:57 -0400

    add missing test for app auth library

--- tests/integration/library/Auth/AppCest.php
+++ tests/integration/library/Auth/AppCest.php
@@ -3,9 +3,11 @@
 namespace Gewaer\Tests\integration\library\Jobs;
 
 use Canvas\Auth\App;
+use Canvas\Hashing\Password;
 use Canvas\Models\Users;
 use Exception;
 use IntegrationTester;
+use PhpParser\Node\Expr\Instanceof_;
 
 class AppCest
 {
@@ -33,16 +35,58 @@ class AppCest
         }
     }
 
-    public function updateUserPassword(IntegrationTester $I)
+    public function updateUserAppPassword(IntegrationTester $I)
     {
         $previousPassword = 'bakatest123567';
         $newPassword = 'bakatest123568';
 
         $user = Users::findFirstOrFail();
-        $userPreviousPassword = $user->passsword;
+        $userAppData = $user->getApp([
+            'conditions' => 'companies_id = :id:',
+            'bind' => [
+                'id' => $user->currentCompanyId()
+            ]
+        ]);
 
-        $I->assertTrue(App::updatePassword($user, $newPassword));
+        $userPreviousPassword = $userAppData->password;
+        $I->assertTrue(App::updatePassword($user, Password::make($newPassword)));
 
-        //we need to compare previous password with new update passwor for the entity
+        $userAppDataAgain = $user->getApp([
+            'conditions' => 'companies_id = :id:',
+            'bind' => [
+                'id' => $user->currentCompanyId()
+            ]
+        ]);
+
+        $I->assertNotEquals($userPreviousPassword, $userAppDataAgain->password);
+
+        //return pass to previous state
+        App::updatePassword($user, Password::make($previousPassword));
+
+        $I->assertEquals($userPreviousPassword, $userAppData->password);
+    }
+
+    /**
+     * Can login to especific app
+     * we run it after updateuserpass to make sure we have the correct pass for this speciifc app
+     *
+     * @param IntegrationTester $I
+     * @return boolean
+     */
+    public function canLoginToSpecificApp(IntegrationTester $I)
+    {
+        try {
+            $user = App::login(
+                'nobody@baka.io',
+                'bakatest123567',
+                true,
+                true,
+                '127.0.0.1'
+            );
+
+            $I->assertTrue($user instanceof Users);
+        } catch (Exception $e) {
+            $I->assertTrue(false);
+        }
     }
 }
