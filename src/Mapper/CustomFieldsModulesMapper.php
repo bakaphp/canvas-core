d09f09bb9a56e75c97e2295bab26b5c27a21d484
Rafael White <rwhite@mctekk.com>
2020-03-13 10:46:19 -0400

    Add custom fields and their value when request is get record of specific id

--- src/Mapper/CustomFieldsModulesMapper.php
+++ src/Mapper/CustomFieldsModulesMapper.php
@@ -7,6 +7,7 @@ namespace Canvas\Mapper;
 use AutoMapperPlus\CustomMapper\CustomMapper;
 use function Canvas\Core\isJson;
 use Phalcon\Mvc\Model\Resultset;
+use Phalcon\Di;
 
 class CustomFieldsModulesMapper extends CustomMapper
 {
@@ -25,30 +26,30 @@ class CustomFieldsModulesMapper extends CustomMapper
         $customFieldsModulesDto->updated_at = $customFieldsModules->updated_at;
         $customFieldsModulesDto->is_deleted = $customFieldsModules->is_deleted;
 
-        foreach ($customFieldsModules->getFields() as $customField) {
+        if (preg_match("/\/([0-9]+)(?=[^\/]*$)/", Di::getDefault()->getRequest()->getURI())) {
+            foreach ($customFieldsModules->getFields() as $customField) {
+                $customFieldsArray['id'] = $customField->getId();
+                $customFieldsArray['apps_id'] = $customField->apps_id;
+                $customFieldsArray['users_id'] = $customField->users_id;
+                $customFieldsArray['companies_id'] = $customField->companies_id;
+                $customFieldsArray['name'] = $customField->name;
+                $customFieldsArray['label'] = $customField->label;
+                $customFieldsArray['custom_fields_modules_id'] = $customField->custom_fields_modules_id;
+                $customFieldsArray['fields_type_id'] = $customField->fields_type_id;
 
-            $customFieldsArray['id'] = $customField->getId();
-            $customFieldsArray['apps_id'] = $customField->apps_id;
-            $customFieldsArray['users_id'] = $customField->users_id;
-            $customFieldsArray['companies_id'] = $customField->companies_id;
-            $customFieldsArray['name'] =  $customField->name;
-            $customFieldsArray['label'] = $customField->label;
-            $customFieldsArray['custom_fields_modules_id'] = $customField->custom_fields_modules_id;
-            $customFieldsArray['fields_type_id'] = $customField->fields_type_id;
+                $customFieldsArray['attributes'] = !empty($customField->attributes) && isJson($customField->attributes) ? json_decode($customField->attributes) : null;
+                $customFieldsArray['values'] = $customField->values ? $this->getValues($customField->values) : null;
+                $customFieldsArray['type'] = $customField->type ? $customField->type->toArray() : null;
 
-            $customFieldsArray['attributes'] = !empty($customField->attributes) && isJson($customField->attributes) ? json_decode($customField->attributes) : null;
-            $customFieldsArray['values'] = $customField->values ? $this->getValues($customField->values) : null;
-            $customFieldsArray['type'] = $customField->type ? $customField->type->toArray() : null;
+                $customFieldsArray['created_at'] = $customField->created_at;
+                $customFieldsArray['updated_at'] = $customField->updated_at;
+                $customFieldsArray['is_deleted'] = $customField->is_deleted;
 
-            $customFieldsArray['created_at'] = $customField->created_at;
-            $customFieldsArray['updated_at'] = $customField->updated_at;
-            $customFieldsArray['is_deleted'] = $customField->is_deleted;
-
-            $customFieldsModulesDto->custom_fields[] = $customFieldsArray;
+                $customFieldsModulesDto->custom_fields[] = $customFieldsArray;
+            }
         }
 
         //This corresponds to the custom fields
-        
 
         return $customFieldsModulesDto;
     }
