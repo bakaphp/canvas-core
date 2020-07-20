39192b4d523a8aa70512669a105b02b6150c16d1
mctekk <max@mctekk.com>
2020-07-19 23:44:20 -0400

    Use new custom fields

--- src/CustomFields/AbstractCustomFieldsModel.php
+++ src/CustomFields/AbstractCustomFieldsModel.php
@@ -5,61 +5,8 @@ namespace Canvas\CustomFields;
 
 use Baka\Contracts\CustomFields\CustomFieldsTrait;
 use Canvas\Models\AbstractModel;
-use Canvas\Models\CustomFieldsModules;
-use PDO;
 
 abstract class AbstractCustomFieldsModel extends AbstractModel
 {
     use CustomFieldsTrait;
-
-    /**
-     * Get all custom fields of the given object.
-     *
-     * @param  array  $fields
-     *
-     * @return array
-     */
-    public function getAllCustomFields(array $fields = [])
-    {
-        //no di? ok so no custom fields
-        if (!$this->di->has('userData')) {
-            return ;
-        }
-
-        //We does it only find names in plural? We need to fix this or make a workaround
-        if (!$models = CustomFieldsModules::findFirstByName($this->getSource())) {
-            return;
-        }
-
-        $bind = [$this->getId(), $this->di->getApp()->getId(), $models->getId(), $this->di->getUserData()->currentCompanyId()];
-
-        // $customFieldsValueTable = $this->getSource() . '_custom_fields';
-        $customFieldsValueTable = $this->getSource() . '_custom_fields';
-
-        //We are to make a new query to replace old Canvas implementation.
-        $result = $this->getReadConnection()->prepare("SELECT l.{$this->getSource()}_id,
-                                               c.id as field_id,
-                                               c.name,
-                                               l.value ,
-                                               c.users_id,
-                                               l.created_at,
-                                               l.updated_at
-                                        FROM {$customFieldsValueTable} l,
-                                             custom_fields c
-                                        WHERE c.id = l.custom_fields_id
-                                          AND l.{$this->getSource()}_id = ?
-                                          AND c.apps_id = ?
-                                          AND c.custom_fields_modules_id = ?
-                                          AND c.companies_id = ? ");
-
-        $result->execute($bind);
-
-        $listOfCustomFields = [];
-
-        while ($row = $result->fetch(PDO::FETCH_OBJ)) {
-            $listOfCustomFields[$row->name] = $row->value;
-        }
-
-        return $listOfCustomFields;
-    }
 }
