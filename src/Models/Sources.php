feb8e3506bede70cd601bfe0b4ada5120c954469
FredPeal <frederickpeal@gmail.com>
2020-08-14 21:59:44 -0400

    Working on social login

--- src/Models/Sources.php
+++ src/Models/Sources.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace Canvas\Models;
 
-use Phalcon\Di;
-use Canvas\Exception\ModelException;
 use Baka\ASDecoder;
 use Canvas\Http\Exception\InternalServerErrorException;
-
+use Facebook\Facebook;
+use Google_Client;
+use Throwable;
 
 /**
- * Class Resources
+ * Class Resources.
  *
  * @package Canvas\Models
  *
@@ -21,7 +21,7 @@ class Sources extends AbstractModel
 {
     /**
      *
-     * @var integer
+     * @var int
      */
     public $id;
 
@@ -39,7 +39,7 @@ class Sources extends AbstractModel
 
     /**
      *
-     * @var integer
+     * @var int
      */
     public $language_id;
 
@@ -57,7 +57,7 @@ class Sources extends AbstractModel
 
     /**
      *
-     * @var integer
+     * @var int
      */
     public $is_deleted;
 
@@ -74,25 +74,27 @@ class Sources extends AbstractModel
      *
      * @return string
      */
-    public function getSource(): string
+    public function getSource() : string
     {
         return 'sources';
     }
 
     /**
-     * Verify if source is Apple
+     * Verify if source is Apple.
      */
-    public function isApple(): bool
+    public function isApple() : bool
     {
         return $this->title == 'apple';
     }
 
     /**
      * Validate Apple User.
+     *
      * @param string $identityToken
+     *
      * @return object
      */
-    public function validateAppleUser(string $identityToken): object
+    public function validateAppleUser(string $identityToken) : object
     {
         $appleUserInfo = ASDecoder::getAppleSignInPayload($identityToken);
 
@@ -101,5 +103,53 @@ class Sources extends AbstractModel
         }
 
         return $appleUserInfo;
+    }
+
+    /**
+     * validation.
+     *
+     * @param  string $email
+     * @param  string $token
+     *
+     * @return bool
+     */
+    public function validation(string $email, string $token) : bool
+    {
+        switch ($this->title) {
+            case 'google':
+                try {
+                    $client = new Google_Client([
+                        'client_id' => getenv('GOOGLE_CLIENT_ID')
+                    ]);
+                    $payload = $client->verifyIdToken($token);
+                    if ($payload) {
+                        $userid = $payload['sub'];
+                        return $payload['email'] === $email;
+                    } else {
+                        return false;
+                    }
+                } catch (Throwable $e) {
+                    return false;
+                }
+                break;
+            case 'facebook':
+                try {
+                    $fb = new Facebook([
+                        'app_id' => getenv('FACEBOOK_APP_ID'),
+                        'app_secret' => getenv('FACEBOOK_APP_SECRET'),
+                        'default_graph_version' => 'v8.0',
+                        // . . .
+                    ]);
+                    $response = $fb->get('/me', $token);
+                    $user = $response->getGraphUser();
+                    if ($user) {
+                        return true;
+                    }
+                    return false;
+                } catch (Exception $e) {
+                    return false;
+                }
+            break;
+        }
     }
 }
