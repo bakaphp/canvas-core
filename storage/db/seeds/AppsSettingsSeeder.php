f9fe9d9a8c2399b4e3eeac70f0a2af3748f105b6
Rafael White <rwhite@mctekk.com>
2020-10-19 16:19:39 -0400

    add default_feeds_comments app settings

--- storage/db/seeds/AppsSettingsSeeder.php
+++ storage/db/seeds/AppsSettingsSeeder.php
@@ -113,6 +113,12 @@ class AppsSettingsSeeder extends AbstractSeed
                 'name' => 'public_images',
                 'value' => '0',
                 'created_at' => date('Y-m-d H:m:s'),
+            ],
+            [
+                'apps_id' => 1,
+                'name' => 'default_feeds_comments',
+                'value' => '3',
+                'created_at' => date('Y-m-d H:m:s'),
             ]
         ];
 
