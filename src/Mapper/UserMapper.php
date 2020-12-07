20a75da158508d1d4146b9276a2ddcaf0738e633
FredPeal <frederickpeal@gmail.com>
2020-12-07 11:42:58 -0400

    Add locations to mapper

--- src/Mapper/UserMapper.php
+++ src/Mapper/UserMapper.php
@@ -94,7 +94,9 @@ class UserMapper extends CustomMapper
         $userDto->user_activation_email = $user->user_activation_email;
         $userDto->welcome = $user->welcome;
         $userDto->photo = $user->photo;
-
+        $userDto->countries = $user->countries ?: [];
+        $userDto->states = $user->states ?: [];
+        $userDto->cities = $user->cities ?: [];
         /**
          * Properties we need to overwrite base on the
          * current app and company the user is running.
