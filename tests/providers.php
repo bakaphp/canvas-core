265153aad9fce3c4116c86d1112f58fefe7a518e
Maximo Castro <max@mctekk.com>
2020-12-13 11:02:43 -0400

    update provider

--- tests/providers.php
+++ tests/providers.php
@@ -4,27 +4,26 @@
  * Enabled providers. Order does matter.
  */
 
+use Canvas\Providers\AclProvider;
+use Canvas\Providers\AppProvider;
 use Canvas\Providers\CacheDataProvider;
 use Canvas\Providers\ConfigProvider;
 use Canvas\Providers\DatabaseProvider;
-use Canvas\Providers\ErrorHandlerProvider;
-use Canvas\Providers\LoggerProvider;
-use Canvas\Providers\ModelsMetadataProvider;
-use Canvas\Providers\SessionProvider;
-use Canvas\Providers\QueueProvider;
-use Canvas\Providers\MailProvider;
-use Canvas\Providers\RedisProvider;
-use Canvas\Providers\RequestProvider;
-use Canvas\Providers\AclProvider;
-use Canvas\Providers\AppProvider;
-use Canvas\Providers\ResponseProvider;
-use Canvas\Providers\FileSystemProvider;
-use Canvas\Providers\EventsManagerProvider;
-use Canvas\Providers\MapperProvider;
 use Canvas\Providers\ElasticProvider;
+use Canvas\Providers\ErrorHandlerProvider;
+use Canvas\Providers\EventsManagerProvider;
+use Canvas\Providers\FileSystemProvider;
+use Canvas\Providers\LoggerProvider;
+use Canvas\Providers\MailProvider;
+use Canvas\Providers\MapperProvider;
 use Canvas\Providers\ModelsCacheProvider;
+use Canvas\Providers\ModelsMetadataProvider;
+use Canvas\Providers\QueueProvider;
+use Canvas\Providers\RedisProvider;
 use Canvas\Providers\RegistryProvider;
-use Canvas\Providers\RouterProvider;
+use Canvas\Providers\RequestProvider;
+use Canvas\Providers\ResponseProvider;
+use Canvas\Providers\SessionProvider;
 
 return [
     ConfigProvider::class,
