4033076ed3e04c52a768a195b7e1b32ec805de41
Maximo Castro <max@mctekk.com>
2021-01-17 20:05:32 -0400

    fix namespace

--- src/Models/SystemModules.php
+++ src/Models/SystemModules.php
@@ -14,16 +14,6 @@ class SystemModules extends BakaSystemModules
 {
     use CustomFieldsTrait;
 
-    public ?string $name = null;
-    public ?string $slug = null;
-    public ?string $model_name = null;
-    public ?int $apps_id = null;
-    public int $parents_id = 0;
-    public ?int $menu_order = null;
-    public int $use_elastic = 0;
-    public ?string $browse_fields = null;
-    public int $show = 0;
-
     /**
      * Initialize method for model.
      */
