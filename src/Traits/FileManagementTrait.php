5e8323da4bbf183e50abfb63d6cd48217a3a8ec3
Rafael White <rwhite@mctekk.com>
2021-01-19 15:27:57 -0400

    Add isset con downloadbale request body variable

--- src/Traits/FileManagementTrait.php
+++ src/Traits/FileManagementTrait.php
@@ -119,7 +119,7 @@ trait FileManagementTrait
     protected function processFiles() : array
     {
         $allFields = $this->request->getPostData();
-        $downloadable = array_key_exists('downloadable', $allFields) && $allFields['downloadable'] ? $allFields['downloadable'] : 0;
+        $downloadable = isset($allFields['downloadable']) ? $allFields['downloadable'] : 0;
 
         $files = [];
         $options = [];
