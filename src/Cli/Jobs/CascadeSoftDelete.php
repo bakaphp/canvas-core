49d0b53912d1a5f7d9674aa0de5b57e32386415e
arfenis <arfen00@gmail.com>
2021-01-28 17:27:05 -0400

    Create job for cascade soft delete

---
+++ src/Cli/Jobs/CascadeSoftDelete.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Canvas\Cli\Jobs;
+
+use Baka\Contracts\Queue\QueueableJobInterface;
+use Baka\Jobs\Job;
+use Phalcon\Di;
+use Phalcon\Mvc\ModelInterface;
+use Throwable;
+
+class CascadeSoftDelete extends Job implements QueueableJobInterface
+{
+    protected ModelInterface $model;
+
+    /**
+     * Constructor.
+     *
+     * @param ModelInterface $model
+     */
+    public function __construct(ModelInterface $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * Handle the cascade soft delete.
+     *
+     * @return bool
+     */
+    public function handle() : bool
+    {
+        $log = Di::getDefault()->get('log');
+        try {
+            $log->info('Cascade Soft Delete ' . get_class($this->model) . ' Id: ' . $this->model->getId());
+
+            $this->model->cascadeSoftDelete();
+
+        } catch (Throwable $e) {
+            $log->error($e->getTraceAsString());
+        }
+
+        return true;
+    }
+}
