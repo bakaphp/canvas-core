c028498cfc50b01dde1b2738a5ecf06a4905e01c
Max Castro <max@mctekk.com>
2021-02-02 18:20:23 -0400

    Update Filesystem.php

--- src/Dto/Filesystem.php
+++ src/Dto/Filesystem.php
@@ -4,63 +4,14 @@ namespace Canvas\Dto;
 
 class Filesystem
 {
-    /**
-     *
-     * @var int
-     */
     public int $id;
-
-    /**
-     *
-     * @var int
-     */
     public int $companies_id;
-
-    /**
-     *
-     * @var int
-     */
     public int $apps_id;
-
-    /**
-     *
-     * @var int
-     */
     public int $users_id;
-
-    /**
-     *
-     * @var string
-     */
     public string $name;
-
-    /**
-     *
-     * @var string
-     */
     public string $path;
-
-    /**
-     *
-     * @var string
-     */
     public string $url;
-
-    /**
-     *
-     * @var string
-     */
     public string $size;
-
-    /**
-     *
-     * @var string
-     */
     public string $file_type;
-
-    /**
-     *
-     * @var array
-     */
-    public array $attributes;
+    public array $attributes = [];
 }
