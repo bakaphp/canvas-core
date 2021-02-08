ce307eef9d3c9b90391d3a1916ae7fd4be322943
Maximo Castro <max@mctekk.com>
2021-02-08 10:33:16 -0400

    Overwrite From Emails

--- src/Providers/MailProvider.php
+++ src/Providers/MailProvider.php
@@ -2,9 +2,9 @@
 
 namespace Canvas\Providers;
 
-use Phalcon\Di\ServiceProviderInterface;
-use Phalcon\Di\DiInterface;
 use Baka\Mail\Manager as BakaMail;
+use Phalcon\Di\DiInterface;
+use Phalcon\Di\ServiceProviderInterface;
 
 class MailProvider implements ServiceProviderInterface
 {
@@ -14,11 +14,22 @@ class MailProvider implements ServiceProviderInterface
     public function register(DiInterface $container) : void
     {
         $config = $container->getShared('config');
+        $app = $container->getShared('app');
 
         $container->setShared(
             'mail',
-            function () use ($config) {
-                $mailer = new BakaMail($config->email->toArray());
+            function () use ($config, $app) {
+                $emailConfig = $config->email->toArray();
+
+                if ($fromEmail = $app->get('email_from_email')) {
+                    $emailConfig['from']['email'] = $fromEmail;
+                }
+
+                if ($fromName = $app->get('email_from_name')) {
+                    $emailConfig['from']['name'] = $fromName;
+                }
+
+                $mailer = new BakaMail($emailConfig);
                 return $mailer->createMessage();
             }
         );
