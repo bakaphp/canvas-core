fb268ddceba590849b3d1227cdc6f17dbb960bee
Max Castro <max@mctekk.com>
2021-03-17 21:00:38 -0400

    Update SocialLoginTrait.php

--- src/Traits/SocialLoginTrait.php
+++ src/Traits/SocialLoginTrait.php
@@ -45,7 +45,7 @@ trait SocialLoginTrait
      * @param string $accessToken
      * @param array $userInfo
      *
-     * @return Array
+     * @return array
      */
     protected function providerLogin(Sources $source, string $identifier, array $userInfo) : array
     {
@@ -54,47 +54,54 @@ trait SocialLoginTrait
             'bind' => [$userInfo['email']]
         ]);
 
-        /**
-         * Lets find if user has a linked source by social network id.
-         */
-        $userLinkedSource = UserLinkedSources::findFirst([
-            'conditions' => 'users_id = ?0 and source_id = ?1 and source_users_id_text = ?2 and is_deleted = 0',
-            'bind' => [
-                $existingUser->id,
-                $source->getId(),
-                $identifier
-            ]
-        ]);
+        if ($existingUser) {
+            /**
+             * Lets find if user has a linked source by social network id.
+             */
+            $userLinkedSource = UserLinkedSources::findFirst([
+                'conditions' => 'users_id = ?0 and source_id = ?1 and source_users_id_text = ?2 and is_deleted = 0',
+                'bind' => [
+                    $existingUser->id,
+                    $source->getId(),
+                    $identifier
+                ]
+            ]);
 
-        /**
-         * This confirms if the linked source exists and if it has a user attached to it. If true then logs in the user.
-         */
-        if ($userLinkedSource) {
-            return $this->loginSocial($userLinkedSource->getUser());
+            /**
+             * This confirms if the linked source exists and if it has a user attached to it. If true then logs in the user.
+             */
+            if ($userLinkedSource) {
+                return $this->loginSocial($userLinkedSource->getUser());
+            }
+
+            /**
+             * If user exists but doesn't have a linked source.
+             */
+            if (!$userLinkedSource) {
+                $newLinkedSource = new UserLinkedSources();
+                $newLinkedSource->users_id = $existingUser->getId();
+                $newLinkedSource->source_id = $source->getId();
+                $newLinkedSource->source_users_id = $identifier;
+                $newLinkedSource->source_users_id_text = $identifier;
+                $newLinkedSource->source_username = ucfirst($source->title) . 'Login-' . $random->base58();
+                $newLinkedSource->saveOrFail();
+
+                return $this->loginSocial($existingUser);
+            }
         }
 
         /**
-         * Whereas if there is no link and no user then lets create a new user and link.
+         * Here if there is no link and no user then lets create a new user and link.
          */
         $random = new Random();
         $password = $random->base58();
 
-        /**
-         * If user exists but doesnt have a linked source.
-         */
-        if (!$userLinkedSource && $existingUser) {
-            $newLinkedSource = new UserLinkedSources();
-            $newLinkedSource->users_id = $existingUser->getId();
-            $newLinkedSource->source_id = $source->getId();
-            $newLinkedSource->source_users_id = $identifier;
-            $newLinkedSource->source_users_id_text = $identifier;
-            $newLinkedSource->source_username = ucfirst($source->title) . 'Login-' . $random->base58();
-            $newLinkedSource->saveOrFail();
-
-            return $this->loginSocial($existingUser);
-        }
-
-        $newUser = $this->createUser($source, $identifier, $userInfo, $password);
+        $newUser = $this->createUser(
+            $source,
+            $identifier,
+            $userInfo,
+            $password
+        );
         return $this->loginUsers($newUser->email, $password);
     }
 
@@ -114,15 +121,16 @@ trait SocialLoginTrait
         $userObj = new Users();
         //Create a new User
 
-        $newUser['firstname'] = $userInfo['firstname'];
-        $newUser['lastname'] = $userInfo['lastname'];
-        $newUser['displayname'] = $userObj->generateDefaultDisplayname();
-        $newUser['password'] = $password;
-        $newUser['email'] = $userInfo['email'];
-        $newUser['user_active'] = 1;
-        $newUser['roles_id'] = 1;
-        $newUser['created_at'] = date('Y-m-d H:m:s');
-        $newUser['defaultCompanyName'] = $newUser['displayname'] . ' Company';
+        $newUser = UserProvider::get();
+        $newUser->firstname = $userInfo['firstname'];
+        $newUser->lastname = $userInfo['lastname'];
+        $newUser->displayname = $userObj->generateDefaultDisplayname();
+        $newUser->password = $password;
+        $newUser->email = $userInfo['email'];
+        $newUser->user_active = 1;
+        $newUser->roles_id = 1;
+        $newUser->created_at = date('Y-m-d H:m:s');
+        $newUser->defaultCompanyName = $userInfo['default_company'] ?? $newUser->displayname . ' Company';
 
         try {
             $this->db->begin();
@@ -148,3 +156,4 @@ trait SocialLoginTrait
         return $user;
     }
 }
+
