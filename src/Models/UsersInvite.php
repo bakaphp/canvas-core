49aec4a800778ecf7ae8c0ed8d9e3164349532c9
Manuel Pellerano <pelleranomanuel@gmail.com>
2021-05-20 12:19:30 -0400

    Change invite process when inviting erased user

--- src/Models/UsersInvite.php
+++ src/Models/UsersInvite.php
@@ -103,7 +103,11 @@ class UsersInvite extends AbstractModel
         ]);
 
         if (is_object($userExists)) {
-            if ($userData->defaultCompany->userAssociatedToCompany($userExists)) {
+            $softDeleteCheck = UsersAssociatedApps::findFirst([
+                'conditions' => 'users_id = ?0 and companies_id = ?1 and is_deleted = 0',
+                'bind' => [$userExists->id, $userData->currentCompanyId()]
+            ]);
+            if ($userData->defaultCompany->userAssociatedToCompany($userExists) && !empty($softDeleteCheck)) {
                 throw new ModelNotFoundException('User already is associated with this company app');
             }
         }
