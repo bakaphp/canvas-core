faf5973efd395dd46ce306862c6306ca60600fd9
Rafael White <rafaelwhite27@gmail.com>
2021-07-28 11:35:05 -0400

    Revert "Revert "Add set default company branch when inviting user already in system""

--- src/Api/Controllers/UsersInviteController.php
+++ src/Api/Controllers/UsersInviteController.php
@@ -149,7 +149,6 @@ class UsersInviteController extends BaseController
         try {
             //Check if user already exists
             $userExists = Users::getByEmail($usersInvite->email);
-            $newUser = $userExists;
             $this->userData->getDefaultCompany()->associate($userExists, $this->userData->getDefaultCompany());
         } catch (Exception $e) {
             try {
