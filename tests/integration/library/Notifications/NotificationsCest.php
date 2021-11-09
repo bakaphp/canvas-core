440265f18762ea239a9a01622133d5199b81d4e0
Jeffry J. Simon <simon.dev93@gmail.com>
2021-11-09 12:54:35 -0400

    test

--- tests/integration/library/Notifications/NotificationsCest.php
+++ tests/integration/library/Notifications/NotificationsCest.php
@@ -8,8 +8,6 @@ use Canvas\Models\Users;
 use Canvas\Notifications\PasswordUpdate;
 use Canvas\Tests\Support\Notifications\NewFollower;
 use IntegrationTester;
-use ApiTester;
-use Page\Data;
 
 class NotificationsCest
 {
@@ -33,35 +31,31 @@ class NotificationsCest
         $user->notify(new NewFollower($users->getFirst()));
     }
 
-    public function grupedNotifications(ApiTester $I)
+    public function grupedNotifications(IntegrationTester $I)
     {
-        for ($i = 0; $i < 20; $i++) {
-            $email = !Users::findFirstByEmail(Data::$defaultEmail) ? Data::$defaultEmail : $I->faker()->email;
-
-            $I->sendPOST(Data::$usersUrl, [
-                'email' => $email,
-                'password' => Data::$defaultPassword,
-                'verify_password' => Data::$defaultPassword,
-                'firstname' => $I->faker()->firstName,
-                'lastname' => $I->faker()->lastName,
-                'displayname' => $I->faker()->userName,
-                'default_company' => $I->faker()->domainWord,
-            ]);
-        }
-
         $users = Users::find();
         $user = Users::findFirstById(1);
 
-        foreach ($users as $userGroup) {
-            for ($i = 0; $i < 10; $i++) {
-                $user->notify(new NewFollower($userGroup, true));
-            }
-        }
+        //if users is less than 10, create 10 users
+        // if ($users->count() < 10) {
+        //     for ($i = 0; $i < 10; $i++) {
+        //         $user = new Users();
+        //         $user->email = 'test' . $i . '@test.com';
+        //         $user->password = 'test';
+        //         $user->save();
+        //     }
+        // }
+
+        // foreach($users as $userGroup) {
+        //     for ($i = 0; $i < 10; $i++) {
+        //         $user->notify(new NewFollower($userGroup, true));
+        //     }
+        // }
 
         $notifications = Notifications::findFirst([
             'order' => 'updated_at DESC'
         ]);
-
+        
         $I->assertJson($notifications->group, 'is a valid json');
         $groupUsers = json_decode($notifications->group);
         $I->assertEquals(count($groupUsers->from_users), 2);
@@ -79,5 +73,8 @@ class NotificationsCest
         ]);
 
         $I->assertNull($notifications->group, 'is not grouped');
+
     }
+
+    
 }
