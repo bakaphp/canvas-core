544c9722e1c44c8ebcd11a4293d030ba9b596595
kaioken <max@mctekk.com>
2021-11-25 12:28:32 -0400

    refact : update user config table

---
+++ storage/db/migrations/20211125162534_update_user_config.php
@@ -0,0 +1,57 @@
+<?php
+
+
+class UpdateUserConfig extends Phinx\Migration\AbstractMigration
+{
+    public function change()
+    {
+        $this->table('user_config', [
+            'id' => false,
+            'primary_key' => ['users_id', 'name'],
+            'engine' => 'InnoDB',
+            'encoding' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'comment' => '',
+            'row_format' => 'COMPACT',
+        ])
+            ->changeColumn('value', 'text', [
+                'null' => true,
+                'default' => null,
+                'limit' => 65535,
+                'collation' => 'utf8mb4_unicode_ci',
+                'encoding' => 'utf8mb4',
+                'after' => 'name',
+            ])
+            ->addIndex(['created_at'], [
+                'name' => 'created_at',
+                'unique' => false,
+            ])
+            ->addIndex(['updated_at'], [
+                'name' => 'updated_at',
+                'unique' => false,
+            ])
+            ->save();
+        $this->table('companies_settings', [
+            'id' => false,
+            'primary_key' => ['companies_id', 'name'],
+            'engine' => 'InnoDB',
+            'encoding' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'comment' => '',
+            'row_format' => 'COMPACT',
+        ])
+            ->addIndex(['created_at'], [
+                'name' => 'created_at',
+                'unique' => false,
+            ])
+            ->addIndex(['updated_at'], [
+                'name' => 'updated_at',
+                'unique' => false,
+            ])
+            ->addIndex(['is_deleted'], [
+                'name' => 'is_deleted',
+                'unique' => false,
+            ])
+            ->save();
+    }
+}
