8b6f3511252a9c550919649dae06bd47fb2e0f48
kaioken <max@mctekk.com>
2022-04-19 12:50:44 -0400

    feat : User Enums

--- src/Enums/App.php
+++ src/Enums/App.php
@@ -15,4 +15,9 @@ class App
     public const DEFAULT_ROLE_SETTING = 'default_admin_role';
     public const DEFAULT_COUNTRY = 'default_user_country';
     public const VERSION = 0.3;
+    public const DEFAULT_TIMEZONE = 'America/New_York';
+    public const DEFAULT_SEX = 'U';
+    public const DEFAULT_USER_LEVEL = 3;
+    public const DEFAULT_LANGUAGE = 'EN';
+    public const DEFAULT_ROLE_ID = 2;
 }
