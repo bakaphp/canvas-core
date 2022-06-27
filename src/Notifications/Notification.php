17c19d568fc8fce85461579336717aa8d71479f0
kaioken <max@mctekk.com>
2022-06-27 09:29:06 -0400

    feat : add companies branch id to notification

--- src/Notifications/Notification.php
+++ src/Notifications/Notification.php
@@ -536,6 +536,7 @@ class Notification implements NotificationInterface
             $this->currentNotification->from_users_id = $this->fromUser->getId();
             $this->currentNotification->users_id = $this->toUser->getId();
             $this->currentNotification->companies_id = $this->fromUser->currentCompanyId();
+            $this->currentNotification->companies_branches_id = $this->fromUser->currentBranchId();
             $this->currentNotification->apps_id = $app->getId();
             $this->currentNotification->system_modules_id = $this->type->system_modules_id;
             $this->currentNotification->notification_type_id = $this->type->getId();
