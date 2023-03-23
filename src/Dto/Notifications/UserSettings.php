1e05c83bb385f58643fd47377acb391f134fdcea
Rafael White <rwhite@mctekk.com>
2023-03-23 11:12:43 -0400

    add null value to description

--- src/Dto/Notifications/UserSettings.php
+++ src/Dto/Notifications/UserSettings.php
@@ -6,7 +6,7 @@ class UserSettings
 {
     public int $users_id;
     public string $name;
-    public string $description;
+    public ?string $description = null;
     public int $notifications_types_id;
     public int $is_enabled = 1;
     public array $channels;
